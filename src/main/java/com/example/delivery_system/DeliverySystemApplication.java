165dae45df345065063a09c07269a9e6d6039a14
Mykhailo Muliar <muliar.misha@gmail.com>
2021-09-13 10:13:41 +0300

    Model mapper bean Disable security for faster swagger-test

--- src/main/java/com/example/delivery_system/DeliverySystemApplication.java
+++ src/main/java/com/example/delivery_system/DeliverySystemApplication.java
@@ -2,12 +2,19 @@ package com.example.delivery_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class DeliverySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeliverySystemApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
